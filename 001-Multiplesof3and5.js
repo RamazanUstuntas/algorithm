289170fd01fb6fc1064e81d4b09f3496bc7a5b65
RamazanUstuntas <ramazan-ustuntas@hotmail.com>
2021-10-20 01:20:41 +0300

    Find the sum of all the multiples of 3 or 5 below 1000.

--- 001-Multiplesof3and5.js
+++ 001-Multiplesof3and5.js
@@ -266,4 +266,104 @@ ReferenceError: i is not defined
 [33m6857[39m
 ]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git status
 On branch master
-Your branch is up to d
+Your branch is up to date with 'origin/master'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	[31m001-Multiplesof3and5.js[m
+	[31m002-EvenFibonacciNumbers.js[m
+	[31m003-LargestPrimeFactor.js[m
+
+nothing added to commit but untracked files present (use "git add" to track)
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git add .
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+	[32mnew file:   001-Multiplesof3and5.js[m
+	[32mnew file:   002-EvenFibonacciNumbers.js[m
+	[32mnew file:   003-LargestPrimeFactor.js[m
+
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git reset
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git sat[K[Ktaturs
+git: 'staturs' is not a git command. See 'git --help'.
+
+The most similar command is
+	status
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git staturs[K[Ks
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	[31m001-Multiplesof3and5.js[m
+	[31m002-EvenFibonacciNumbers.js[m
+	[31m003-LargestPrimeFactor.js[m
+
+nothing added to commit but untracked files present (use "git add" to track)
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git add 001-Multiplesof3and5.js 
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git commit -m ""f"İ"N"D" "[1P"[1P"[1P"[1P"i"n"d" "t"h"e" "[1P"[1P"[1P"[1P"[1P"[1P"[1P"[1P"[1P"Find the sum of all the multiples of 3 or 5 below 1000."
+[master c65c0c5] Find the sum of all the multiples of 3 or 5 below 1000.
+ 1 file changed, 269 insertions(+)
+ create mode 100644 001-Multiplesof3and5.js
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git push
+Enumerating objects: 4, done.
+Counting objects:  25% (1/4)Counting objects:  50% (2/4)Counting objects:  75% (3/4)Counting objects: 100% (4/4)Counting objects: 100% (4/4), done.
+Delta compression using up to 4 threads
+Compressing objects:  33% (1/3)Compressing objects:  66% (2/3)Compressing objects: 100% (3/3)Compressing objects: 100% (3/3), done.
+Writing objects:  33% (1/3)Writing objects:  66% (2/3)Writing objects: 100% (3/3)Writing objects: 100% (3/3), 1.92 KiB | 1.92 MiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To github.com:RamazanUstuntas/algorithm.git
+   46b087e..c65c0c5  master -> master
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	[31m002-EvenFibonacciNumbers.js[m
+	[31m003-LargestPrimeFactor.js[m
+
+nothing added to commit but untracked files present (use "git add" to track)
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git add 002-EvenFibonacciNumbers.js 
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git commit -m ""find the sum of the even-valued terms.[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cfind the sum of the even-valu[1P[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cfeven-val[1@u[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cieven-va[1@l[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cbeven-v[1@a[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Coeven-[1@v[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cneven[1@-[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Caeve[1@n[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Ccev[1@e[A]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cce[1@v[1@i[1@ [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C"
+[master 0121fe0] find the sum of the fibonacci even-valued terms.
+ 1 file changed, 22 insertions(+)
+ create mode 100644 002-EvenFibonacciNumbers.js
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git push
+Enumerating objects: 4, done.
+Counting objects:  25% (1/4)Counting objects:  50% (2/4)Counting objects:  75% (3/4)Counting objects: 100% (4/4)Counting objects: 100% (4/4), done.
+Delta compression using up to 4 threads
+Compressing objects:  33% (1/3)Compressing objects:  66% (2/3)Compressing objects: 100% (3/3)Compressing objects: 100% (3/3), done.
+Writing objects:  33% (1/3)Writing objects:  66% (2/3)Writing objects: 100% (3/3)Writing objects: 100% (3/3), 719 bytes | 719.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To github.com:RamazanUstuntas/algorithm.git
+   c65c0c5..0121fe0  master -> master
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	[31mmodified:   001-Multiplesof3and5.js[m
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	[31m003-LargestPrimeFactor.js[m
+
+no changes added to commit (use "git add" and/or "git commit -a")
+]0;siradanpil@siradanpilPC: ~/algorithm[01;32msiradanpil@siradanpilPC[00m:[01;34m~/algorithm[00m$ gi[K[Kgit status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	[31mmodified:   001-Multiplesof3and5.js[m
+
+Untracked files:
+  (use "git add <file>..." to include in what will be c
